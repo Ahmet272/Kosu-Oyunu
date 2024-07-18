b430d03651d8c9940764a87d806bb046cd0624c3
Ahmet Altunbaş <140913760+Ahmet272@users.noreply.github.com>
2024-07-18 13:57:19 +0300

    Vıdeo2Fınısh

---
+++ Assets/Script/Alt_karakter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alt_karakter : MonoBehaviour
+{
+    GameObject Target;
+    NavMeshAgent _Navmesh;
+    void Start()
+    {
+        _Navmesh = GetComponent<NavMeshAgent>();
+        Target = GameObject.FindWithTag("GameManager").GetComponent<GameManager>().VarisNoktasi;
+    }
+
+    private void LateUpdate()
+    {
+        _Navmesh.SetDestination(Target.transform.position);
+    }
+}
